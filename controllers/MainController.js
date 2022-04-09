73b91620fddb021e6892532e8ca58f07426c5b65
alok9038 <kumaralok1884@gmail.com>
2022-04-09 11:50:28 +0530

    completed

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -2,12 +2,12 @@ const todoModel = require("../models/todoModel");
 // var dateTime = require('node-datetime');
 const dateTime = require('date-and-time');
 const now = new Date();
-var time = dateTime.format(now, 'hh:mm A');   // => '07:14 AM GMT+0000'
+var time = dateTime.format(now, 'DD MMM hh:mm A');   // => '07:14 AM GMT+0000'
 
 
 async function HomePage(req, res){
     let todo = await todoModel.find({}).sort({'status':1})
-    let taskCount = await todoModel.countDocuments()
+    let taskCount = await todoModel.find({"status":0}).countDocuments()
 
     var newDate = dateTime.format(now, 'ddd, DD MMM YYYY');       // => 'Fri, Jan 02 2015'
 
@@ -53,9 +53,9 @@ function removeTask(req, res){
     })
 }
 
-async function updateTask(req, res){
+function updateTask(req, res){
     let id = req.params.id
-    let update = await todoModel.findByIdAndUpdate({_id:id},{task:req.body.task}, function(error, response){
+    todoModel.findByIdAndUpdate({_id:id},{task:req.body.task}, function(error, response){
         if(error) return error
         
         return res.redirect('/')
